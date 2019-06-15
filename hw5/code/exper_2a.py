1f70a9b9c0c95a5e4c8a1b02f97183fcf9c21d1c
zihao12 <wangzh@uchicago.edu>
2019-06-14 22:41:16 -0500

    small changes for arraqy job

--- hw5/code/exper_2a.py
+++ hw5/code/exper_2a.py
@@ -3,6 +3,7 @@ from gibbs import data_pre, initialization, evaluation, gibbs_sampler
 import time
 import numpy as np
 import itertools
+import pickle
 
 
 ss = [1,10,100,1000]
@@ -16,10 +17,22 @@ print("s = {}; beta = {}; g = {}\n".format(s,beta,g))
 
 (Xs, Btruth) = data_pre()
 start = time.time()
-Bs = gibbs_sampler(Xs,g = g, s= s, beta = beta, niter=100)
+Bs = gibbs_sampler(Xs,g = g, s= s, beta = beta, niter=1)
 runtime = time.time() - start
 print("runtime : {}".format(runtime))
 
 acc = evaluation(Bs, Btruth)
+
 print("acc : {}".format(acc))
+
+out = {}
+out["s"] = s
+out["beta"] = beta
+out["g"] = g
+out["acc"] = acc
+
+## save result
+out_name = "../output/exper_2a_{}.pkl".format(idx)
+with open(out_name, "wb") as f:
+	pickle.dump(out, f)
 
