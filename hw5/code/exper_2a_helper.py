649de46f991b15dce91845dab6390a6865082394
zihao12 <wangzh@uchicago.edu>
2019-06-14 23:32:35 -0500

    helper

---
+++ hw5/code/exper_2a_helper.py
@@ -0,0 +1,26 @@
+## analyze result from exper_2a
+import pickle 
+import numpy as np
+import pandas as pd
+
+ss =[]
+betas = []
+gs = []
+accs = []
+
+for i in range(64):
+	outname = "../output/exper_2a_{}.pkl".format(i)
+	with open(outname, "rb") as f:
+		out = pickle.load(f)
+		ss.append(out["s"])
+		betas.append(out["beta"])
+		gs.append(out["g"])
+		accs.append(out["acc"])
+
+out_df = pd.DataFrame(
+    {'s': ss,
+     'beta': betas,
+     'g': gs,
+     'acc': accs
+    })
+
